73a403ea541bafc06e8dece5b61dfe4a722b7a7e
ko100lomiwe <podshivalovalex@gmail.com>
2017-03-28 19:31:55 +0300

    Initial commit

---
+++ Client/src/main/java/app/Main.java
@@ -0,0 +1,24 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @SuppressWarnings("all")
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(this.getClass().getClassLoader().getResource("sample.fxml"));
+        primaryStage.setTitle("Клиент ну очень тонкий");
+        primaryStage.setScene(new Scene(root, 1000, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
